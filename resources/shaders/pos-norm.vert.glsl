a17496833fbd0ab0f56acaf08376474b9458dfb6
Vladislav Pavlov (ISh-) <imwaves@gmail.com>
2025-01-26 14:09:57 +0100

    fix(PhongShader): mult normals by model

--- resources/shaders/pos-norm.vert.glsl
+++ resources/shaders/pos-norm.vert.glsl
@@ -12,8 +12,8 @@ out vec3 vNormal;
 
 void main() {
 	vFragPos = vec3(model * vec4(aPos, 1.0));
-	// vNormal = mat3(transpose(inverse(model))) * aNormal;
-	vNormal = aNormal;
+	vNormal = mat3(transpose(inverse(model))) * aNormal;
+	// vNormal = aNormal;
 	// TexCoords = aTexCoords;
 
 	gl_Position = projection * view * vec4(vFragPos, 1.0);
