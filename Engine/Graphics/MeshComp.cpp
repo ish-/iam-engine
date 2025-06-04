40da4e6f40c06d9ed7f71ca34102964aea212a56
Vladislav Pavlov (ISh-) <imwaves@gmail.com>
2025-06-04 02:11:57 +0200

    ...save 7 Mesh works

--- Engine/Graphics/MeshComp.cpp
+++ Engine/Graphics/MeshComp.cpp
@@ -14,43 +14,18 @@ Symbol MeshComp::getASystemType () {
   return symbol;
 }
 
-MeshComp::MeshComp(const Conf& conf): conf(conf), AComp() {
+MeshComp::MeshComp(const Conf& conf): Mesh(conf), AComp() {
 
 }
 
-void MeshComp::draw() {
-  glBindVertexArray(geo->VAO);
 
-  if (material) {
-    material->bind();
-  }
-
-  glDrawElements(GL_TRIANGLES, geo->vertexCount, GL_UNSIGNED_INT, 0);
-
-  glBindVertexArray(0);
-}
 
 void MeshComp::update (const float& dt) {
 
 }
 
 void MeshComp::init() {
-  material = PhongMaterial::getPtr();
-
-  if (geo)
-    return;
-
-  if (conf.path.length() > 0) {
-    auto modelFull = AssetStore::get().loadModel(conf.path);
-    geo = modelFull->geo;
-    material = modelFull->material;
-  }
-  else
-    geo = BoxGeo::getPtr();
-
-  if (conf.autoInstancing)
-    geo->instancesCount++;
-
+  Mesh::initMesh();
 }
 
 // TODO: cache it since it's not update()d
@@ -61,60 +36,8 @@ mat4 MeshComp::getAbsTransformMatrix() const {
   return getTransformMatrix();
 }
 
-#include <Engine.hpp>
 MeshComp::~MeshComp () {
   if (conf.autoInstancing && geo) {
     geo->instancesCount--;
   }
-}
-
-bool MeshComp::shouldInstance () { return conf.autoInstancing && getInstancesNum() > 1; }
-bool MeshComp::shouldCulled () { return shouldInstance() ? conf.instancingCulling : conf.culling; }
-
-BoundingBox MeshComp::getBoundingBox() {
-  BoundingBox bb;
-  if (conf.culling == CullingMode::NONE) {
-    return bb;
-  }
-
-  auto transform = getAbsTransformMatrix();
-  // bad transform
-  // bb.min = glm::vec4(geo->boundingBox.min, 1) * transform;
-  // bb.max = glm::vec4(geo->boundingBox.max, 1) * transform;
-
-  // just position
-  if (conf.culling == CullingMode::SPHERE) {
-    vec3 pos = Transform(transform).getPosition();
-    bb.min = geo->boundingBox.min + pos;
-    bb.max = geo->boundingBox.max + pos;
-    bb.update();
-    return bb;
-  }
-
-  if (conf.culling == CullingMode::BB) {
-    auto& localBB = geo->boundingBox;
-    bb.min = glm::vec3(FLT_MAX);
-    bb.max = glm::vec3(-FLT_MAX);
-
-    for (int x = 0; x <= 1; ++x) {
-      for (int y = 0; y <= 1; ++y) {
-        for (int z = 0; z <= 1; ++z) {
-          glm::vec3 corner = glm::vec3(
-            x ? localBB.max.x : localBB.min.x,
-            y ? localBB.max.y : localBB.min.y,
-            z ? localBB.max.z : localBB.min.z
-          );
-          glm::vec3 worldCorner = glm::vec3(transform * glm::vec4(corner, 1.0f));
-          bb.min = glm::min(bb.min, worldCorner);
-          bb.max = glm::max(bb.max, worldCorner);
-        }
-      }
-    }
-    bb.update();
-    return bb;
-  }
-}
-
-int MeshComp::getInstancesNum () const {
-  return geo->instancesCount;
 }
--- Engine/Graphics/MeshComp.hpp
+++ Engine/Graphics/MeshComp.hpp
@@ -6,63 +6,30 @@
 #include "../Actor.hpp"
 #include "../ILifecycle.hpp"
 #include "AssetStore.hpp"
+#include "Mesh.hpp"
 
 using namespace std;
 
 class Symbol;
-class Shader;
-class Geo;
 
-enum CullingMode {
-  NONE = 0,
-  SPHERE = 1,
-  BB = 2,
-};
-
-class MeshComp : public AComp, public ILifecycle, public Transform {
+class MeshComp : public AComp, public ILifecycle, public Mesh {
 public:
+  using Conf = Mesh::Conf;
   virtual Symbol getASystemType () override;
-
-  struct Conf {
-    glm::vec3 tint = glm::vec3(1.);
-    bool visible = true;
-    bool shaded = true;
-    bool wireframe = false;
-    bool invertNormals = false;
-    bool autoInstancing = true;
-    bool instancingCulling = false;
-    CullingMode culling = CullingMode::SPHERE;
-
-    string path = "";
-
-    JSON_DEFINE_OPTIONAL(Conf, tint, visible, shaded, wireframe, invertNormals, autoInstancing, path);
-  };
-
-  Conf conf;
 
   MeshComp(): AComp() {}
   MeshComp(const Conf& conf);
 
-  shared_ptr<Geo> geo;
-  sp<class Material> material;
-  bool culled = false;
-
-  virtual void draw();
   // virtual void drawInstances(const vector<mat4>& transforms);
   // virtual void drawInstances();
+
+  vector<sp<Mesh>> children;
+
+  mat4 getAbsTransformMatrix() const;
 
   virtual void init() override;
 
   void update (const float& dt) override;
-
-  // TODO: cache it since it's not update()d
-  mat4 getAbsTransformMatrix() const;
-
-  virtual bool shouldInstance ();
-  virtual bool shouldCulled ();
-  virtual int getInstancesNum () const;
-
-  virtual class BoundingBox getBoundingBox();
 
   ~MeshComp ();
 };
